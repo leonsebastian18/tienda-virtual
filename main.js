6059eca71daa337501f72698f4018832e6259298
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2023-09-28 14:44:27 -0500

    se organizo el menu mobile y el desktop

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+burguerIcon.addEventListener('click',toogleMobileMenu )
 
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
